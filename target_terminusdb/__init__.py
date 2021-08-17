4c69370a55ec9b4f3bba572f72dfcc25b27c7e0d
Cheukting <cheukting.ho@gmail.com>
2021-08-17 15:01:21 +0100

    use _load_setting and _connect form Python client

--- target_terminusdb/__init__.py
+++ target_terminusdb/__init__.py
@@ -17,6 +17,7 @@ import singer
 
 from terminusdb_client import WOQLClient
 from terminusdb_client.errors import DatabaseError
+from terminusdb_client.scripts.scripts import _connect, _load_settings
 from terminusdb_client.woqlschema import LexicalKey
 
 logger = singer.get_logger()
@@ -45,15 +46,8 @@ def persist_lines(config, lines):
     headers = {}
     validators = {}
 
-    client = WOQLClient(config["server"])
-    client.connect()
-    try:
-        client.create_database(config["database"])
-    except DatabaseError as error:
-        if "Database already exists." in str(error):
-            client.connect(db=config["database"])
-        else:
-            raise InterfaceError(f"Cannot connect to {config['database']}.")
+    client, _ = _connect(config)
+
     # Initize list for terminusdb client
     class_dict_list = []
     doc_dict_list = []
@@ -87,7 +81,7 @@ def persist_lines(config, lines):
             # If the record needs to be flattened, uncomment this line
             # flattened_record = flatten(o['record'])
 
-            # TODO: Process Record message here..
+            # Process Record message here..
 
             doc_dict = {'@type': o['stream']}
             doc_dict.update(o['record'])
@@ -108,7 +102,7 @@ def persist_lines(config, lines):
                 raise Exception("key_properties field is required")
             key_properties[stream] = o['key_properties']
 
-            # TODO: Process Schema message here..
+            # Process Schema message here..
 
             class_dict = {"@type": "Class", "@id": stream, "@key": {"@type": "Lexical", "@fields": o['key_properties']}}
             for key, value in o['schema']["properties"].items():
@@ -160,15 +154,8 @@ def main():
     args = parser.parse_args()
 
     if args.config:
-        if args.config.split('.')[-1] == 'json':
-            with open(args.config) as input:
-                config = json.load(input)
-        elif args.config.split('.')[-1] == 'py':
-            sys.path.append(os.getcwd())
-            _temp = __import__(args.config.split('.')[0], globals(), locals(), ["SERVER", "DATABASE"], 0)
-            server = _temp.SERVER
-            database = _temp.DATABASE
-            config = {"server": server, "database": database}
+        with open(args.config) as input:
+            config = _load_settings(args.config)
     else:
         config = {}
 
