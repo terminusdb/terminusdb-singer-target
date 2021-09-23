3a7050203982974cd80f5a423c957b4434e7f702
Cheukting <cheukting.ho@gmail.com>
2021-09-23 13:34:16 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ for old_str, new_str in replace_pairs.items():
 
 setup(
     name="target-terminusdb",
-    version="0.1.0",
+    version="0.1.1",
     description="Singer.io target for inserting data in TerminusDB",
     long_description=long_description,
     long_description_content_type="text/markdown",
